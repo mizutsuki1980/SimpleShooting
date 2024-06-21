5995e9062d4469f969d4f2cb409a146c9ac460cf
mizutsuki1980 <mizutuki1980@gmail.com>
2024-06-21 09:17:19 +0900

    自機の前にブロックを表示、弾になる予定

--- app/src/main/java/com/example/simpleshooting/MyCustomView.kt
+++ app/src/main/java/com/example/simpleshooting/MyCustomView.kt
@@ -18,17 +18,47 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
         p.color = Color.RED
         p.style = Paint.Style.FILL
         canvas.drawRect(r, p)
-
-        //この中ではposX,yが使えるのは間違いない。ちゃんと四角は動いている。
-        //ここで別の変数にいれてみてはどうか？
         val rMyShikaku = Rect(posX, posY, posX+100, posY+100)
         val pMyShikaku = Paint()
 
-        pMyShikaku.color = Color.BLUE
+        if (frame % 2 ==1) {
+            pMyShikaku.color = Color.BLUE
+        }else {
+            pMyShikaku.color = Color.YELLOW
+        }
+        //目チカチカすんで
+
+
+        if (frame % 10 ==1) {
+            pMyShikaku.color = Color.RED
+        }
+
+        //球をだす、、とは？？？
+        //一度だけ座標を覚えておいて、それをｙのみプラスしつづけていく
+        //だからtamaIchiの座標にposXを使い続けてはだめなんだろうなぁ
+
+        val tamaIchi = Rect(posX+45, posY-45, posX+10, posY-10)
+        val tamaPaint = Paint()
+        tamaPaint.color = Color.WHITE
+        if (frame % 10 ==1) {
+            tamaPaint.style = Paint.Style.FILL
+        }else{
+            tamaPaint.style = Paint.Style.STROKE
+        }
+        //塗りをなしにして消えたように見せらんねー？と思ったが
+
+        //見せたくないものは描画しなければいいのか
+        if (frame % 10 ==1) {
+            canvas.drawRect(tamaIchi, tamaPaint)
+        }
+
+
         pMyShikaku.style = Paint.Style.FILL
         canvas.drawRect(rMyShikaku, pMyShikaku)
 
-    }
+        }
+
+
     var frame = 0
     fun tsugiNoSyori() {
         frame += 1
@@ -40,8 +70,6 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
     fun beginAnimation() {
         tsugiNoSyori()
     }
-
-
 
     var posX = 150
     var posY = 150
