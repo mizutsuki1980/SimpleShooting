8e34f0135a9291b8db2a4a541773f7ae9bc32a95
mizutsuki1980 <mizutuki1980@gmail.com>
2024-06-25 08:34:31 +0900

    最後にタップした場所のｘ、ｙをテキストで表示するように変更

--- app/src/main/java/com/example/simpleshooting/MainActivity.kt
+++ app/src/main/java/com/example/simpleshooting/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simpleshooting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,16 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val custom = findViewById<MyCustomView>(R.id.mycustom)
         custom.post { custom.beginAnimation() }
-        //なんで        custom.post { custom.beginAnimation() }　って書くんですかね？
-        //custom.beginAnimation()　じゃないんだ。
-        //custom.post{ } って何？
-
-//        findViewById<TextView>(R.id.textLabelX).text=custom.post {custom.posX}.toString()
-        findViewById<TextView>(R.id.textLabelY).text=custom.posY.toString()
-        //ここは更新されてない気がする。だから最初の値のままなんじゃないかな。
-        //なんでここもhandlerを使えばいいんじゃないかな。
-
-
-
+        findViewById<Button>(R.id.setButton).setOnClickListener {
+            findViewById<TextView>(R.id.textLabelX).text=custom.posX.toString()
+            findViewById<TextView>(R.id.textLabelY).text=custom.posY.toString()
+        }
     }
 }
