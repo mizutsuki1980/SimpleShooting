7de49fd1e32a5592ec5e73baf78108861a322ad1
mizutsuki1980 <mizutuki1980@gmail.com>
2024-06-25 12:47:32 +0900

    弾を一発のみ発射するように変更

--- app/src/main/java/com/example/simpleshooting/MyCustomView.kt
+++ app/src/main/java/com/example/simpleshooting/MyCustomView.kt
@@ -11,52 +11,41 @@ import android.view.View
 import android.widget.TextView
 
 class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-    var hueru = 0
+    var tamaX = 0
+    var tamaY = 0
     override fun onDraw(canvas: Canvas) {
-        //tsugiNoSyoriに置いても、onDrawに置いてもうごくのか
-        //どっちに置くのがいいのだろう？↓↓↓
         frame += 1
-
-        val r = Rect(100, 100, 200+hueru, 200+hueru)
+        val r = Rect(100, 100, 200, 200)
         val p = Paint()
         p.color = Color.RED
         p.style = Paint.Style.FILL
         canvas.drawRect(r, p)
-        hueru += 5
-
-        val rMyShikaku = Rect(posX, posY, posX+100, posY+100)
-        val pMyShikaku = Paint()
+        val ookisa = 100
+        val jikiIchi = Rect(posX-ookisa/2, posY-ookisa/2, posX+ookisa/2, posY+ookisa/2)
+        val jikiIro = Paint()
 
         if (frame % 2 ==1) {
-            pMyShikaku.color = Color.BLUE
+            jikiIro.color = Color.WHITE
         }else {
-            pMyShikaku.color = Color.YELLOW
+            jikiIro.color = Color.GRAY
         }
 
-        pMyShikaku.style = Paint.Style.FILL
-        canvas.drawRect(rMyShikaku, pMyShikaku)
-
-        var xxx = 0
-        var yyy = 0
-
-        //まずは一発撃つ
-        if (frame % 10 ==1) {
-            val tamaPaint = Paint()
-            //弾一発だすのは成功。
-            tamaPaint.color = Color.WHITE
-            tamaPaint.style = Paint.Style.FILL
-
-            if (xxx == 0){
-            xxx = posX
-            yyy = posY
-            }
-
-            val tamaIchi = Rect(xxx+40, yyy-frame*10-60, xxx+60, yyy-frame*10-40)
-            canvas.drawRect(tamaIchi, tamaPaint)
-            //じゃぁ2発は？
+        jikiIro.style = Paint.Style.FILL
+        canvas.drawRect(jikiIchi, jikiIro)
 
 
+        val tamaOokisa = 40
+        val tamaPaint = Paint()
+        tamaPaint.color = Color.WHITE
+        tamaPaint.style = Paint.Style.FILL
+
+        if (tamaX == 0){
+            tamaX = posX
+            tamaY = posY
         }
+        val kasoku = frame*30
+        val tamaIchi = Rect(tamaX+tamaOokisa, tamaY-kasoku-(tamaOokisa+20), tamaX+tamaOokisa+20, tamaY-kasoku-tamaOokisa)
+        canvas.drawRect(tamaIchi, tamaPaint)
      }
 
 
@@ -74,23 +63,26 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
     var posY = 150
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_DOWN) {
+            tamaX = 0
+            tamaY = 0
             posX = event.x.toInt()
             posY = event.y.toInt()
-            hueru = 0
             return true // 処理した場合はtrueを返す約束
         }
 
         if (event.action == MotionEvent.ACTION_UP) {
+            tamaX = 0
+            tamaY = 0
             posX = event.x.toInt()
             posY = event.y.toInt()
-            hueru = 0
             return true // 処理した場合はtrueを返す約束
         }
 
         if (event.action == MotionEvent.ACTION_MOVE) {
+            tamaX = 0
+            tamaY = 0
             posX = event.x.toInt()
             posY = event.y.toInt()
-            hueru = 0
             return true // 処理した場合はtrueを返す約束
         }
         return super.onTouchEvent(event)
