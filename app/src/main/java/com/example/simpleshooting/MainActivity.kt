610bebae7453639da865c315cff2006f2aad8dcf
mizutsuki1980 <mizutuki1980@gmail.com>
2024-06-25 09:02:23 +0900

    ｘ、ｙを常に自動で入るように変更

--- app/src/main/java/com/example/simpleshooting/MainActivity.kt
+++ app/src/main/java/com/example/simpleshooting/MainActivity.kt
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
+    val handler = android.os.Handler()
+    lateinit var xxx : MyCustomView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val custom = findViewById<MyCustomView>(R.id.mycustom)
         custom.post { custom.beginAnimation() }
+        xxx = custom
         findViewById<Button>(R.id.setButton).setOnClickListener {
             findViewById<TextView>(R.id.textLabelX).text=custom.posX.toString()
             findViewById<TextView>(R.id.textLabelY).text=custom.posY.toString()
         }
+        mainTsugiNoSyori()
+
+
     }
+
+    fun mainTsugiNoSyori() {
+        findViewById<TextView>(R.id.textLabelX).text=xxx.posX.toString()
+        findViewById<TextView>(R.id.textLabelY).text=xxx.posY.toString()
+        handler.postDelayed( { mainTsugiNoSyori() }, 100)
+    }
+
 }
