36a9cae3acc88792dda65fed68cb75b866073676
mizutsuki1980 <mizutuki1980@gmail.com>
2024-08-06 10:40:55 +0900

    失敗した。弾が自機から離れない。

--- app/src/main/java/com/example/simpleshooting/MyCustomView.kt
+++ app/src/main/java/com/example/simpleshooting/MyCustomView.kt
@@ -10,14 +10,25 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 
-//こっちはクラス
-class myJikinoUgoki(posXx:Int,posYy:Int,myookisa:Int){
 
-    val left = posXx - myookisa / 2
-    val top = posYy - myookisa / 2
-    val right = posXx + myookisa / 2
-    val bottom = posYy + myookisa / 2
-}
+    class myJikinoUgoki(posXx:Int,posYy:Int,myookisa:Int){
+        val left = posXx - myookisa / 2
+        val top = posYy - myookisa / 2
+        val right = posXx + myookisa / 2
+        val bottom = posYy + myookisa / 2
+    }
+
+    class myTamanoUgoki(posXx:Int,posYy:Int,myTamaNoOkisa:Int,myFrame:Int,tamasokudo:Int,tamaFrame:Int){
+        val tamaX = posXx
+        val tamaY = posYy
+        val susumu = tamaFrame * tamasokudo
+        val xx = tamaX - myTamaNoOkisa / 2
+        val xxx = xx + myTamaNoOkisa
+        val yy = tamaY - susumu - (myTamaNoOkisa / 2)
+        val yyy = yy + myTamaNoOkisa
+    }
+
+
 
 class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     var tamaX = 0
@@ -30,24 +41,13 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
     var tamaPaint = Paint()
     var tamaList = mutableListOf(0,0,0,0,1,1,1,1,2,2,2,2)
 
-    //こっちは関数
-    fun myJiki(posXx:Int,posYy:Int,myookisa:Int):Int{
-        val myleft = posXx - myookisa / 2
-        return myleft
-        val myTop = posY - myookisa / 2
-    }
+
 
     override fun onDraw(canvas: Canvas) {
         val ookisa = 100
-
-        val mmm = myJiki(posX,posY,ookisa)
-        val mmmm = myJikinoUgoki(posX,posY,ookisa)
-
-        val jikiIchi = Rect(mmmm.left, mmmm.top, mmmm.right, mmmm.bottom)
+        val myJiki = myJikinoUgoki(posX,posY,ookisa)
+        val jikiIchi = Rect(myJiki.left, myJiki.top, myJiki.right, myJiki.bottom)
         val jikiIro = Paint()
-
-
-
         jikiIro.style = Paint.Style.FILL
         jikiIro.color = Color.WHITE
         canvas.drawRect(jikiIchi, jikiIro)
@@ -57,34 +57,29 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
         tamaIchiPaint.style = Paint.Style.FILL
         tamaIchiPaint.color = Color.RED
 
+
+
         //弾①の処理
 
-
+        //tamaFrameはそのまま使って書いてみる
         if (tamaFrame>=1) {
-            if (tamaX == 0) {
-                tamaX = posX
-                tamaY = posY
-                tamaFrame = 1
-            }
-            susumu = tamaFrame * tamasokudo
-            val xx = tamaX - tamaOokisa / 2
-            val xxx = xx + tamaOokisa
-            val yy = tamaY - susumu - (tamaOokisa / 2)
-            val yyy = yy + tamaOokisa
-            tamaList[0] = xx
-            tamaList[1] = yy
-            tamaList[2] = xxx
-            tamaList[3] = yyy
-            val tamaIchi = Rect(tamaList[0], tamaList[1], tamaList[2], tamaList[3])
+            //posXx:Int,posYy:Int,myTamaNoOkisa:Int,myFrame:Int,tamasokudo:Int,tamaFrame:Int
+            var myTama = myTamanoUgoki(posX,posY,ookisa,frame,tamasokudo,tamaFrame)
+            susumu = myTama.susumu
+            val tamaIchi = Rect(myTama.xx, myTama.yy, myTama.xxx, myTama.yyy)
             canvas.drawRect(tamaIchi, tamaIchiPaint)
-
             tamaFrame += 1
-
             if (tamaY - susumu < 1) {
                 tamaX = 0
                 tamaFrame = 1
             }
+
+
+
         }
+
+
+
     }
 
 
