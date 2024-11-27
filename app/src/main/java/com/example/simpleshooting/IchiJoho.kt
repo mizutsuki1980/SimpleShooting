49f820c1a7025fbde53a335a537e035a5d4aa6ac
mizutsuki1980 <mizutuki1980@gmail.com>
2024-11-27 10:48:25 +0900

    class IchiJohoを別のファイルへ移した

--- app/src/main/java/com/example/simpleshooting/IchiJoho.kt
+++ app/src/main/java/com/example/simpleshooting/IchiJoho.kt
@@ -2,7 +2,7 @@ package com.example.simpleshooting
 import android.graphics.Paint
 import android.graphics.Rect
 
-class IchiJouhou(var x:Int, var y:Int, var Ookisa:Int, val tamaOokisa:Int){
+class IchiJoho(var x:Int, var y:Int, var Ookisa:Int, val tamaOokisa:Int){
     var alive = true    //念のため　使うのかわからないけど
     var homing = true   //敵の弾が自機を捕まえに来るのに使う
     var zenkaiVect = mutableListOf<Int>(0,0)    //敵の弾が自機を捕まえに来るのに使う
@@ -12,7 +12,6 @@ class IchiJouhou(var x:Int, var y:Int, var Ookisa:Int, val tamaOokisa:Int){
     var top = y  - (Ookisa) /2
     var bottom = y + (Ookisa) /2
     var iro = Paint()
-
 
     fun shikakuRectXY(x:Int,y:Int,Ookisa:Int): Rect {
         left = x  - Ookisa / 2
--- app/src/main/java/com/example/simpleshooting/MyCustomView.kt
+++ app/src/main/java/com/example/simpleshooting/MyCustomView.kt
@@ -29,27 +29,6 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
     var et2 = eTama()
 
 
-    class IchiJoho(var x:Int, var y:Int, var Ookisa:Int, val tamaOokisa:Int){
-        var alive = true    //念のため　使うのかわからないけど
-        var homing = true   //敵の弾が自機を捕まえに来るのに使う
-        var zenkaiVect = mutableListOf<Int>(0,0)    //敵の弾が自機を捕まえに来るのに使う
-
-        var left = x  - Ookisa / 2
-        var right = x  + Ookisa / 2
-        var top = y  - (Ookisa) /2
-        var bottom = y + (Ookisa) /2
-        var iro = Paint()
-
-
-        fun shikakuRectXY(x:Int,y:Int,Ookisa:Int):Rect{
-            left = x  - Ookisa / 2
-            right = x  + Ookisa / 2
-            top = y  - Ookisa / 2
-            bottom = y + Ookisa / 2
-            val m = Rect(left, top, right,bottom)
-            return m
-        }
-    }
 
 
     override fun onDraw(canvas: Canvas) {
