390dbacc6a71f974ade9c4a5410525dcc70d0a50
mizutsuki1980 <mizutuki1980@gmail.com>
2024-12-04 10:38:42 +0900

    jTamaを変更　ここで起動しなくなった

--- app/src/main/java/com/example/simpleshooting/MyCustomView.kt
+++ app/src/main/java/com/example/simpleshooting/MyCustomView.kt
@@ -188,7 +188,7 @@ class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attr
     }
 
     fun jTama():IchiJoho{
-        val m = IchiJoho(m.x,m.y,10,10)
+        val m = IchiJoho(jk.m.x,jk.m.y,10,10)
         m.iro.style = Paint.Style.FILL
         m.iro.color = Color.GREEN
         return m
