dbc8006e1fd317a4b2af3f286c85f3f814407fb7
mizutsuki1980 <mizutuki1980@gmail.com>
2024-12-13 11:39:58 +0900

    とりあえず保存

--- app/src/main/java/com/example/simpleshooting/JikiJoho.kt
+++ app/src/main/java/com/example/simpleshooting/JikiJoho.kt
@@ -4,34 +4,24 @@ import android.graphics.Color
 import android.graphics.Paint
 
 class JikiJoho(var jikiOokisa:Int,var jikiX:Int,var jikiY:Int,var m :IchiJoho ) {
-
+    //色情報はJikiJohoに入れることにする　iro
+    //m.ってやつを調べていたら、eTamaというのを修正しなければいけないのでは？と思った。
+    //IchiJohoで作っちゃったのは、今全部m.に色が入ってるのか。面倒くさいっぽい。
+    //まずは自機の色を使ってないか。調べる。
+    var x:Int
+    var y:Int
     var iro = Paint()
 
-    // var jikiOokisa
-    // var jikiX
-    // var jikiY
-    // var m :IchiJoho //(IchiJouhouのjiki())
-    var x :Int
-    var y : Int
-    //ということは、いうことは、、、全部作り直し？
-
-    //なんとなくわかった、最初の数値をinitで決めれるのか。それがオブジェクトに入る。それをプロパティで取り出して使う、と。
-    //あとから変更も可能、と。
     init {
+        x=jikiX
+        y=jikiY
+
         iro.style = Paint.Style.FILL
         iro.color = Color.RED
         val m = IchiJoho(0,0,50,10)
-         x  =250
-         y  =250
         jikiOokisa = 50
-
-
         m.iro.style = Paint.Style.FILL
         m.iro.color = Color.RED
-        //ここで、書いたものは、いったいどういう動きをするんだろうか？
-        //return とか使えないよな。どこにどう格納されんだよ。
-        //        println("最初の初期化ブロックによる $jikiOokisa のプリント")
-
     }
 
 
