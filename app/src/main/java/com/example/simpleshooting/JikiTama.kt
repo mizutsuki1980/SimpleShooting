338622df9a94fa0fe2734dd992bdc013a23a725a
mizutsuki1980 <mizutuki1980@gmail.com>
2024-12-20 12:32:23 +0900

    MyCustomViewのjTamaをJikiTamaに置き換える

--- app/src/main/java/com/example/simpleshooting/JikiTama.kt
+++ app/src/main/java/com/example/simpleshooting/JikiTama.kt
@@ -2,19 +2,27 @@ package com.example.simpleshooting
 
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 
-class JikiTama(var x:Int,var y:Int,ookisa:Int) {
-    //var x:Int
-    //var y:Int
-    //IchiJohoってやつはいるのか？
-
-
-    fun jTama(jikiX:Int,jikiY:Int,jikiTamaOkisa:Int):IchiJoho{
-        //弾をリセットする。最初の状態にする。
-        val z = IchiJoho(jikiX,jikiY,10,jikiTamaOkisa)
-        z.iro.style = Paint.Style.FILL
-        z.iro.color = Color.GREEN
-        return z
+class JikiTama(var x:Int,var y:Int) {
+    var iro = Paint()
+    var ookisa:Int
+    init {
+        ookisa = 10
+        iro.style = Paint.Style.FILL
+        iro.color = Color.GREEN
     }
+
+
+
+    fun shikakuRectXY(): Rect {
+        val left = x  - ookisa / 2
+        val right = x  + ookisa / 2
+        val top = y  - ookisa / 2
+        val bottom = y + ookisa / 2
+        val m = Rect(left, top, right,bottom)
+        return m
+    }
+
 
 }
