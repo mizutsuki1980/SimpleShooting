73080be04e105dfbb5e25a3b2c89e146b4a44354
mizutsuki1980 <mizutuki1980@gmail.com>
2024-12-24 10:03:04 +0900

    よくわからないのでとりあえず戻してみた。とりあえず保存。

--- app/src/main/java/com/example/simpleshooting/JikiTama.kt
+++ app/src/main/java/com/example/simpleshooting/JikiTama.kt
@@ -29,36 +29,28 @@ class JikiTama(var x:Int,var y:Int) {
     fun tamaSyori(jikiX:Int,jikiY:Int){
         val tamaSpeed = 8.0
         val tamaPlus = 10 * tamaSpeed .toInt()
-
         tamaFrameFFF += 1
-
         y -= tamaPlus
-
-
         if(y<5){
             tamaFrameFFF=0
         }        //画面の上部で消える
-
         if(tamaFrameFFF==20){
             tamaFrameFFF=0
         }        //20フレームでリセット
-
       if(tamaFrameFFF==0){
-            //jt = JikiTama(jiki.x,jiki.y)
-            //自機の弾のリセット処理をここにかく。ｘ、ｙは自機の位置になる。なんで自機位置を入れないとだめ？
             x=jikiX
             y=jikiY
         }
-
     }
 
     fun tamaJikiSyori(jikiX:Int,jikiY:Int,ex:Int,ey:Int,atariKyori:Int):Boolean{
-        var hit = false
         if(ookisa == 30){
             x=jikiX
             y=jikiY
+            tamaFrameFFF=0
         }
 
+        var hit = false
         val vx = x - ex
         val vy = y - ey
 
@@ -67,12 +59,6 @@ class JikiTama(var x:Int,var y:Int) {
             iro.color = Color.WHITE
             ookisa = 30
             hit = true
-            //e = teki()
-            //敵が消える処理　敵を消しちゃうのはなー、この中の処理じゃないだろう。多分。
-
-            //scoreCount += 1
-            //スコアカウント処理　スコアもこの中にあると困るんではないのかな。
-
         }
         return hit
     }
