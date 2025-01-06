b784800304a1880e7a8391e560c289138ba0212c
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-06 10:12:06 +0900

    Tekiも複数Drawしてみる

--- app/src/main/java/com/example/simpleshooting/Teki.kt
+++ app/src/main/java/com/example/simpleshooting/Teki.kt
@@ -13,12 +13,16 @@ class Teki {
 
     var tekiSpeed = 10
     var iro = Paint()
+    var irosub = Paint()
 
     init{
         x = 20
         y = 100
         iro.style = Paint.Style.FILL
         iro.color = Color.CYAN
+
+        irosub.style = Paint.Style.FILL
+        irosub.color = Color.BLUE
 
     }
 
@@ -31,11 +35,24 @@ class Teki {
         return m
     }
 
+    fun shikakuRectXYSub(xxx:Int,yyy:Int,ooookisa:Int): Rect {
+        val left = xxx  - ooookisa / 2
+        val right = xxx  + ooookisa / 2
+        val top = yyy  - ooookisa / 2
+        val bottom = yyy + ooookisa / 2
+        val m = Rect(left, top, right,bottom)
+        return m
+    }
     fun yokoIdo(){
         x += tekiSpeed
         if(x >= 800) { x = -50 } //端っこだったら元の位置に戻る
     }
     fun draw(canvas: Canvas){
+         //なるほどー、shikakuRectXY()こういう風に書いちゃうと位置が固定されてしまうのか。
         canvas.drawRect(shikakuRectXY(), iro)   //敵の移動　処理
+        canvas.drawRect(shikakuRectXYSub(x-20-ookisa/2,y,30), irosub)   //敵の移動　処理
+        canvas.drawRect(shikakuRectXYSub(x+20+ookisa/2,y,30), irosub)   //敵の移動　処理
+
+
     }
 }
