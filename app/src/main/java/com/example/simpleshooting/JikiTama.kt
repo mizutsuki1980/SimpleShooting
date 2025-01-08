5d64b405bb2d1671ea1bc4e574cee29a04b1d7f3
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-08 19:06:41 +0900

    JikiTamaのnextFrame()を修正

--- app/src/main/java/com/example/simpleshooting/JikiTama.kt
+++ app/src/main/java/com/example/simpleshooting/JikiTama.kt
@@ -23,55 +23,59 @@ class JikiTama(var x:Int,var y:Int) {
         iro.color = Color.GREEN
     }
 
-
     fun nextFrame(jiki:Jiki,teki:Teki) {
         when(status) {
             TAMA_NASI_STATE -> {
-                // 現在の自機の場所に移動してリセット
-                x = jiki.x
-                y = jiki.y
-                iro.color = Color.GREEN
-                hit = false
-
-                // 移動状態に
-                status = NORMAL_STATE
+                jikiKaraStart(jiki)         // 現在の自機の場所に移動してリセット
             }
-
             NORMAL_STATE -> {
-                //ひとつ上に弾を移動
-                val tamaSpeed = 8.0
-                val tamaPlus = 10 * tamaSpeed.toInt()
-                y -= tamaPlus //自機の弾を上方向に動かす
-
-                //当たっているかチェック
-                val vx = x - teki.x
-                val vy = y - teki.y
-                val atariKyori = 5 + teki.ookisa / 2 //当たり判定の距離
-
-                if (vx < atariKyori && vx > -atariKyori && vy < atariKyori && vy > -atariKyori) {
-                    //当たっていたら状態遷移
-                    hit = true
-                    status = TAMA_HIT_STATE
-                    iro.color = Color.WHITE
-                    ookisa = 30
-                }
-
-                // 画面外に出たら無しの状態に一旦遷移
-                if (y < 5) {
-                    status = TAMA_NASI_STATE
-                }
+                moveOne()                //ひとつ上に弾を移動
+                attaterukaCheck(teki)    //当たっているかチェック
+                if (y < 5) { status = TAMA_NASI_STATE } // 画面外に出たら無しの状態に一旦遷移
             }
             TAMA_HIT_STATE -> {
-                // ここで何フレームかカウントしてからでもいいが、とりあえず1フレームで次に行く
-                hit = false //ヒットは１回のみカウントするので、すぐにfalseに
-                status = TAMA_HIT_END_STATE
-
+                hitCountSyori() //ヒット処理して次へ
             }
             TAMA_HIT_END_STATE -> {
-                // もとに戻す
-                ookisa = 10
-                status = TAMA_NASI_STATE
+                motoniModosu()  // もとに戻す
             }
+        }
+    }
+    fun moveOne(){
+        val tamaSpeed = 8.0
+        val tamaPlus = 10 * tamaSpeed.toInt()
+        y -= tamaPlus //自機の弾を上方向に動かす
+    }
+    fun jikiKaraStart(jiki:Jiki){
+        x = jiki.x
+        y = jiki.y
+        iro.color = Color.GREEN
+        hit = false
+        status = NORMAL_STATE
+    }
+    fun motoniModosu(){
+        hit = false //ヒットは１回のみカウントするので、すぐにfalseに
+        ookisa = 10
+        status = TAMA_NASI_STATE
+
+    }
+    fun hitCountSyori(){
+        hit = true //ヒットは１回のみカウントするので、すぐにfalseに
+        status = TAMA_HIT_END_STATE
+    }
+
+    fun attaterukaCheck(teki:Teki):Boolean {
+        val vx = x - teki.x
+        val vy = y - teki.y
+        val atariKyori = 5 + teki.ookisa / 2 //当たり判定の距離
+
+        if (vx < atariKyori && vx > -atariKyori && vy < atariKyori && vy > -atariKyori) {
+            status = TAMA_HIT_STATE
+            iro.color = Color.WHITE
+            ookisa = 30
+            return true
+        }else {
+            return false
         }
     }
 
