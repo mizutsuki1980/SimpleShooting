46aebe98770d19dc372c4a4a8e547940b11af081
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-10 10:40:32 +0900

    HPCounterでshikakuRect()とhpShikakuRect()でHPの四角をつくるよう修正 draw内でforを使い表示

--- app/src/main/java/com/example/simpleshooting/HPCounter.kt
+++ app/src/main/java/com/example/simpleshooting/HPCounter.kt
@@ -24,6 +24,11 @@ class HPCounter {
     //今回は120を6で割ることになる。20だ。
     //10+20+10+20+10+20+10+20+10+20+10+20+10という感じか。
 
+    //んー、なんかよくわからんくなってきた。
+    //表示するときにはCanavasがいるから、Drawの中で繰り返さなきゃならんのか。
+    //for(a in 1..<6) {　とかで描画しながら増やすとかやっていきたかったが
+    //最初にリスト作るほうがいいのか
+
 
     var yokohaba = 190
     var tatehaba = 30
@@ -41,101 +46,40 @@ class HPCounter {
         //irosub.strokeWidth = 5.0f
 
     }
-    fun drawOne(canvas: Canvas,rect:Rect,iro:Paint) {
-        canvas.drawRect(rect, iro)
-    }
-
-    fun shikakuRect(canvas:Canvas,atai:Int){
-        val m = Rect(x, y, x+yokohaba,y+tatehaba)
-        drawOne(canvas,m, iro)
-        //まず元の四角を表示
-
-        //HPの分だけ四角を表示
-        for(a in 1..<6) {
-            val m = Rect(x+10+20*a, y, x+10+20*a+20,y+tatehaba)
-            drawOne(canvas,m, iro)
-
-        }
-
-    }
-
-
-
     fun shikakuRect(): Rect {
-        var xxx = 600
-        var yyy = 0
-        var ooookisa = 200
-        val left = 500
-        val right = 690
-        val top = 0
-        val bottom = 50
+        //元になる四角をつくる、ここはOK
+        val left = x
+        val right = x+190//690
+        val top = y
+        val bottom = y+50 //50
         val m = Rect(left, top, right,bottom)
         return m
     }
 
-    fun shikakuRect1(): Rect {
-        val left = 510
-        val right = 530
-        val top = 10
-        val bottom = 40
+    fun hpShikakuRect(hp:Int): Rect {
+        //1なら、、、？   10              と   10+20
+        //2なら、、、、   10+20+10    　   と　10+20+10+20　
+        //3なら、、、、   10+20+10+20+10　 と　10+20+10+20+10+20
+        val hidarikata = x + waku*hp + 20*(hp-1)
+        val migikata =  hidarikata+20
+        val left = hidarikata
+        val right = migikata
+
+
+        val top = y + waku
+        val bottom = y+50 - waku //50
         val m = Rect(left, top, right,bottom)
         return m
     }
 
-    fun shikakuRect2(): Rect {
-        val left = 540
-        val right = 560
-        val top = 10
-        val bottom = 40
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-
-    fun shikakuRect3(): Rect {
-        val left = 570
-        val right = 590
-        val top = 10
-        val bottom = 40
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-
-    fun shikakuRect4(): Rect {
-        val left = 600
-        val right = 620
-        val top = 10
-        val bottom = 40
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-    fun shikakuRect5(): Rect {
-        val left = 630
-        val right = 650
-        val top = 10
-        val bottom = 40
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-    fun shikakuRect6(): Rect {
-        val left = 660
-        val right = 680
-        val top = 10
-        val bottom = 40
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
 
 
 
     fun draw(canvas: Canvas){
         canvas.drawRect(shikakuRect(), iro)
-        canvas.drawRect(shikakuRect1(), irosub)
-        canvas.drawRect(shikakuRect2(), irosub)
-        canvas.drawRect(shikakuRect3(), irosub)
-        canvas.drawRect(shikakuRect4(), irosub)
-        canvas.drawRect(shikakuRect5(), irosub)
-        canvas.drawRect(shikakuRect6(), irosub)
-
+        for(a in 0..<4) {
+            canvas.drawRect(hpShikakuRect(a+1), irosub)
+        }
     }
 
 }
