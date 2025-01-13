6ab2cd5ba1e2f69b367d66d8dcd4e46625c1c5bc
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-13 13:08:19 +0900

    JikiTamaのattaterukaCheck　コメントを削除

--- app/src/main/java/com/example/simpleshooting/JikiTama.kt
+++ app/src/main/java/com/example/simpleshooting/JikiTama.kt
@@ -71,28 +71,8 @@ class JikiTama(var x:Int,var y:Int) {
         hit = true //ヒットは１回のみカウントするので、すぐにfalseに
         status = TAMA_HIT_END_STATE
     }
-    fun attaterukaCheckVec(teki:Teki):Boolean {
-        val vx = x - teki.x
-        val vy = y - teki.y
-        val v = Math.sqrt((vx * vx) + (vy * vy) .toDouble())
-        val atarikyori = (teki.ookisa-10).toDouble()
-        //なんか当たってないのに当たってるように見えるので、－10してる。円だから？
-        //となると、ベクトルを使うと、なんかいまいちになるのか。へー
-        if (v < atarikyori){
-            return true
-        }else{
-            return false
-        }
-
-    }
-
 
     fun attaterukaCheck(teki:Teki):Boolean {
-        //いろいろ作って思ったが、あれ、これってベクトルじゃね？
-        val vx = x - teki.x
-        val vy = y - teki.y
-        val atariKyori = 5 + teki.ookisa / 2 //当たり判定の距離
-
         val x1 = teki.x -teki.ookisa / 2
         val y1 = teki.y -teki.ookisa / 2
         val x2 = teki.x +teki.ookisa / 2
@@ -109,46 +89,6 @@ class JikiTama(var x:Int,var y:Int) {
             return false
         }
 
-        //ｖｘっていうのは敵ｘと弾ｘの差
-        //ｖｙっていうのは敵ｙと弾ｙの差
-        //マイナスもありうる
-
-        //ａｔａｒｉ距離ってのは敵の大きさの半分、つまりａｔａｒｉ距離ではない。敵の大きさの半分
-        //ａｔａｒｉ範囲ならａｔａｒｉ範囲ｘ１、ｙ１、ｘ２、ｙ２ときめちまいえばいいべ
-
-        //敵はこんな風にきめている
-        //        fun shikakuRectXY(): Rect {
-        //            val left = x  - ookisa / 2
-        //            val right = x  + ookisa / 2
-        //            val top = y  - ookisa / 2
-        //            val bottom = y + ookisa / 2
-        //            val m = Rect(left, top, right,bottom)
-        //            return m
-        //        }
-        //これが敵の表示されている部分。ここに入ったらあたりになる。
-        //なんでｘ１（左肩）は敵ｘーｏｏｋｉｓａ/２とかわかってる。
-
-
-
-        //んん－これは何をチェックしているんだ。たぶん距離だけど。
-
-        //ｖｘがａｔａｒｉ距離より小さくて
-        //ｖｘがマイナスのａｔａｒｉ 距離より小さくて
-        //さらに
-        //ｖｙがａｔａｒｉ距離より小さくて
-        //ｖｙがマイナスのａｔａｒｉ距離より小さい
-        //if,ifっていっぱい作った方がいいってことか。まぁそうなのかも。
-
-
-        // でも全部条件に一致しないとダメってことだよなぁ。flagを二つ使うとか？ｘ軸はオッケー、ｙ軸はオッケーみたいな。
-        //あー、なるほど、ＸとかＹがマイナスなんじゃないかってところをカバーしてるのか。でも座標ってプラスしかないよね。
-        //ちがうな。ちゃんとマイナスを考えているっぽい。消したら左側が全部あたりになってしまった。
-
-        //この一文に全部はいってるのすげーな。なんなん。
-        //たぶん、右上より左下、左上より右下、右下より左上、左下より右上、全部に該当するもの、ってことかな。
-        //敵の位置きまってます、ｘｙ　から　ｘ＋？？？ｙ＋？？？
-        //弾の位置きまってますｘ、ｙ
-        //これを比較しているだけ
     }
 
 
