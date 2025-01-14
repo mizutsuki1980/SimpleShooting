17f8e5a1db745329e2a847ae913cc06d87b831c7
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-14 10:23:33 +0900

    古いバージョンに戻した。

--- app/src/main/java/com/example/simpleshooting/TekiTamaRef.kt
+++ app/src/main/java/com/example/simpleshooting/TekiTamaRef.kt
@@ -12,8 +12,8 @@ class TekiTamaRef(jiki:Jiki, teki:Teki) {
     var y = teki.y
     val iro = Paint()
     var ookisa:Int
+    var homing :Boolean
     var hit :Boolean
-    var isFirst :Boolean
     var zenkaix : Int
     var zenkaiy : Int
     var speed : Double
@@ -27,27 +27,18 @@ class TekiTamaRef(jiki:Jiki, teki:Teki) {
     var kisekiAto_x : Int
     var kisekiAto_y : Int
 
-
-
-    val TAMA_NASI_STATE = 1
-    val NORMAL_STATE = 2
-    val TAMA_HIT_STATE = 3
-    val TAMA_HIT_END_STATE = 4
-
-    var status = TAMA_NASI_STATE // 最初は玉が画面内に無い状態
-
-    var kakudo = 0.0
-
     init{
         ookisa = 10
         iro.style = Paint.Style.FILL
         iro.color = Color.BLUE
+        homing = true
         hit = false
-        isFirst = true
         zenkaix = teki.x
         zenkaiy = teki.y
+
         kisekiMae_x = zenkaix
         kisekiMae_y = zenkaiy
+
         kisekiAto_x = kisekiMae_x
         kisekiAto_y = kisekiMae_y
 
@@ -63,51 +54,27 @@ class TekiTamaRef(jiki:Jiki, teki:Teki) {
         irosubAto.strokeWidth = 1.5f
     }
 
-
-    fun tekiKaraStart(teki:Teki){
-        ookisa = 10
-        iro.style = Paint.Style.FILL
-        iro.color = Color.BLUE
-        hit = false
-        isFirst = true
-        x = teki.x
-        y = teki.y
-        zenkaix = teki.x
-        zenkaiy = teki.y
-
-        kisekiMae_x = zenkaix
-        kisekiMae_y = zenkaiy
-
-        kisekiAto_x = kisekiMae_x
-        kisekiAto_y = kisekiMae_y
-
-        speed = 3.0
-
-        status = NORMAL_STATE
-    }
-    fun kisekiWoTukuru(){
-        zenkaix = x
-        zenkaiy = y
+    fun move(jiki:Jiki){
         kisekiAto_x = kisekiMae_x
         kisekiAto_y = kisekiMae_y
         kisekiMae_x = x
         kisekiMae_y = y
 
-    }
-    fun kakudoTukuru(vx:Int,vy:Int){
-        if (isFirst) {
-            kakudo = Math.sqrt((vx * vx) + (vy * vy).toDouble())
-            isFirst = false
-        }
-    }
 
-    fun moveOne(jiki:Jiki){
-        kisekiWoTukuru()    //最初に段々計算入れる、これは要る
+
         val xhanai =650
         val yHani = 900
         var vx = jiki.x - x
         var vy = jiki.y - y
 
+        val resetKyori = 500 //よけ始める距離
+        if(vx<resetKyori && vx > -resetKyori && vy<resetKyori && vy > -resetKyori){ homing = false }
+        if (homing == false) {
+            vx = zenkaix
+            vy = zenkaiy
+        }
+        zenkaix = vx
+        zenkaiy = vy
         //敵の弾の移動
         val v = Math.sqrt((vx * vx) + (vy * vy) .toDouble())
         x += ((vx / v)*10 * speed).toInt()
@@ -116,112 +83,20 @@ class TekiTamaRef(jiki:Jiki, teki:Teki) {
         if (y > yHani || y < 0){zenkaiy = -vy}
     }
 
-
-    fun moveOne2(jiki:Jiki){
-        //ｘ、ｙが変化する前に、これらに数値を入れることで軌跡のデータになる。
-        kisekiWoTukuru()
-        var vx = jiki.x - x
-        var vy = jiki.y - y
-        kakudoTukuru(vx,vy)
-        val v = kakudo
-        x += ((vx / v)*10 * speed).toInt()
-        y += ((vy / v)*10 * speed).toInt()
-
-        //ここ謎、なんでzennkaiにマイナスで入れてんの？でもこれないとダメ。
-        if (x > 650 || x < 0){zenkaix = -vx }
-        if (y > 900 || y < 0){zenkaiy = -vy}
-
-    }
-
-    fun moveOne1(jiki:Jiki){
-        // なんだかよくわからなくなってる
-        //まず最初だけ角度が決まる。
-        //敵位置から自機位置に向かう。
-        //そのあとは角度は変わらない。ずっと同じ角度で動き続ける。
-        //kakudoはDouble
-        // んーなんか意味わからんくなってきた。作り直すか。
-
-        kisekiAto_x = kisekiMae_x
-        kisekiAto_y = kisekiMae_y
-        kisekiMae_x = x
-        kisekiMae_y = y
-
-        val xhanai =650
-        val yHani = 900
-
-        var vx = jiki.x - x
-        var vy = jiki.y - y
-
-        if (isFirst) {
-            kakudo = Math.sqrt((vx * vx) + (vy * vy).toDouble())
-            isFirst = false
-        }
-        val v = kakudo
-
-        vx = zenkaix
-        vy = zenkaiy
-
-        zenkaix = vx
-        zenkaiy = vy
-
-        //最初だけ角度が決める
-        x += ((vx / v)*10 * speed).toInt()
-        y += ((vy / v)*10 * speed).toInt()
-        if (x > xhanai || x < 0){zenkaix = -vx }
-        if (y > yHani || y < 0){zenkaiy = -vy}
-    }
-
-    fun attaterukaCheck(jiki:Jiki):Boolean {
+    fun atariCheck(jiki:Jiki){
         val vx = x - jiki.x
         val vy = y - jiki.y
-        val kyori = Math.sqrt((vx * vx) + (vy * vy) .toDouble())
-        val atarikyori = (jiki.ookisa).toDouble()
-        if (kyori < atarikyori){
-            return true
+        if(ookisa == 30){
+            hit = true
         }else{
-            return false
-        }
-    }
-
-
-    fun gotoHitState(){
-        iro.color = Color.DKGRAY
-        ookisa = 30
-        status = TAMA_HIT_STATE
-    }
-    fun hitCountSyori(){
-        hit = true
-        status = TAMA_HIT_END_STATE
-    }
-    fun motoniModosu(){
-        hit = false //hitだけ先に戻さないと二回カウントされてしまう。
-        status = TAMA_NASI_STATE
-    }
-
-
-    fun nextFrame(jiki:Jiki,teki:Teki) {
-        when(status) {
-            TAMA_NASI_STATE -> {
-                tekiKaraStart(teki)         //最初のリセット処理
-            }
-            NORMAL_STATE -> {
-                moveOne(jiki)                //自機にひとつ近づくように弾を移動
-                if(attaterukaCheck(jiki)) {                     //自機に当たっているかチェック
-                    gotoHitState()
-                }
-                //画面外なら、最初へ状態遷移
-                if (x > 690 || x < 0 || y > 1050 || y < 0){ status = TAMA_NASI_STATE }
-            }
-            TAMA_HIT_STATE -> {
-                moveOne(jiki)   //めりこむ感じ
-                hitCountSyori() //ヒット処理して次へ
-            }
-            TAMA_HIT_END_STATE -> {
-                moveOne(jiki)   //めりこむ感じ
-                motoniModosu()  // もとに戻す
+            val atariKyori = jiki.atariKyori()
+            if (vx < atariKyori && vx > -atariKyori && vy < atariKyori && vy > -atariKyori) {
+                iro.color = Color.DKGRAY
+                ookisa = 30
             }
         }
     }
+
 
     fun shikakuRectXY(): Rect {
         val left = x  - ookisa / 2
