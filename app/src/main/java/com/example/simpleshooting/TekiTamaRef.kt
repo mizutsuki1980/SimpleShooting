597fdbcd4c86e42bc3aa61aac42efe2136e44365
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-14 12:10:38 +0900

    ｖｘ、ｖｙをholdx、holdyに変更

--- app/src/main/java/com/example/simpleshooting/TekiTamaRef.kt
+++ app/src/main/java/com/example/simpleshooting/TekiTamaRef.kt
@@ -10,99 +10,79 @@ class TekiTamaRef(jiki:Jiki, teki:Teki) {
     //reflection
     var x = teki.x
     var y = teki.y
-    val iro = Paint()
     var ookisa:Int
     var isfirst :Boolean
     var hit :Boolean
     var spx : Int
     var spy : Int
-    var speed : Double
+    val speed = 3.0
 
+    val iro = Paint()
     val irosubMae = Paint()
     val irosubAto = Paint()
 
     //軌跡の計算に使う
-    var x2 : Int
-    var y2 : Int
-    var x3 : Int
-    var y3 : Int
+    var x2 = x
+    var y2 = y
+    var x3 = x
+    var y3 = y
 
     //反射の計算に使う
-    var vx = 0
-    var vy = 0
+    var holdx = 0
+    var holdy = 0
 
     init{
+        x = teki.x
+        y = teki.y
         ookisa = 10
-        iro.style = Paint.Style.FILL
-        iro.color = Color.BLUE
-
-
         isfirst = true
-
         hit = false
-        spx = teki.x
-        spy = teki.y
-
-        x2 = spx
-        y2 = spy
-
-        x3 = x2
-        y3 = y2
-
-        speed = 3.0
+        spx = x
+        spy = y
 
         //線を設定
+        iro.style = Paint.Style.FILL
+        iro.color = Color.BLUE   //argb(255, 255, 255, 200)
+
         irosubMae.style = Paint.Style.STROKE
         irosubMae.color = Color.BLUE   //argb(255, 255, 255, 200)
         irosubMae.strokeWidth = 4.0f
-
         irosubAto.style = Paint.Style.STROKE
         irosubAto.color = Color.BLUE   //argb(255, 255, 255, 200)
         irosubAto.strokeWidth = 1.5f
     }
 
-    fun move(jiki:Jiki){
+    fun kisekiKeisan(){
         x3 = x2
         y3 = y2
         x2 = x
         y2 = y
+    }
 
-        val xhanai =650
-        val yHani = 900
-
+    fun move(jiki:Jiki){
+        kisekiKeisan()
         if (isfirst) {
-             vx = jiki.x - x
-             vy = jiki.y - y
+             holdx = jiki.x - x
+             holdy = jiki.y - y
             isfirst = false
         }else {
-             vx = spx
-             vy = spy
+            //ここに入るともうｖｘは動かない。反転するときにだけマイナス１で掛け算になる。
+             holdx = spx
+             holdy = spy
         }
+        spx = holdx
+        spy = holdy
 
-        spx = vx
-        spy = vy
-
-        val kyori = Math.sqrt((vx * vx) + (vy * vy) .toDouble())
+        val kyori = Math.sqrt((holdx * holdx) + (holdy * holdy) .toDouble())
         x += ((spx / kyori)*10 * speed).toInt()
         y += ((spy / kyori)*10 * speed).toInt()
 
-        if (x > xhanai || x < 0){spx = -vx }
-        if (y > yHani || y < 0){spy = -vy}
+        val xhanai =650
+        val yHani = 900
+        if (x > xhanai || x < 0){spx = -holdx }
+        if (y > yHani || y < 0){spy = -holdy}
     }
 
-    fun atariCheck(jiki:Jiki){
-        val vx = x - jiki.x
-        val vy = y - jiki.y
-        if(ookisa == 30){
-            hit = true
-        }else{
-            val atariKyori = jiki.atariKyori()
-            if (vx < atariKyori && vx > -atariKyori && vy < atariKyori && vy > -atariKyori) {
-                iro.color = Color.DKGRAY
-                ookisa = 30
-            }
-        }
-    }
 
 
     fun shikakuRectXY(): Rect {
