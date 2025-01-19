e6790c038f88d17f953d5350e2b2b168d3fa13db
mizutsuki1980 <mizutuki1980@gmail.com>
2025-01-19 20:13:49 +0900

    剣の修正　とりあえず保存

--- app/src/main/java/com/example/simpleshooting/JikiKen.kt
+++ app/src/main/java/com/example/simpleshooting/JikiKen.kt
@@ -24,97 +24,18 @@ class JikiKen(jiki:Jiki) {
         iro.style = Paint.Style.FILL
         iro.color = Color.WHITE
     }
-    fun tatenagashikakuRectXY(): Rect {
-        val left = x  - ookisa / 2
-        val right = x  + ookisa / 2
-
-        nobiruhanni=250
-        val top = y  - nobiruhanni
-        val bottom = y
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-
-    fun tatenagashikakuRectXY2(): Rect {
-        val left = x  - ookisa / 2
-        val right = x  + ookisa / 2
-        if (nobiruhanni<1){nobiruhanni=0}
-        if (nobiruhanni>250){nobiruhanni=250}
-        val top = y  - nobiruhanni
-        val bottom = y
-        val m = Rect(left, top, right,bottom)
-        return m
-    }
-    fun draw(canvas: Canvas){
-            canvas.drawRect(tatenagashikakuRectXY(), iro)  //自機
-    }
-
-    fun draw2(canvas: Canvas){
-        if(timeCount>10) {
-            canvas.drawRect(tatenagashikakuRectXY(), iro)  //自機
-        }
-    }
 
 
-    fun jikiKaraStart(jiki:Jiki){
-        x = jiki.x
-        y = jiki.y
-        nobiruhanni = 10
-        timeCount = 0
-        hit = false
-        status =  NORMAL_STATE
-        iro.color = Color.WHITE
-    }
-
-    fun taiki(jiki:Jiki){
-        timeCount += 1
-        if(timeCount==10){
-            x = jiki.x
-            y = jiki.y
-        }
-
-    }
-
-    //なんかわかんないからデバックモードとしてだしっぱにしたいな
-    fun nobiru(jiki:Jiki) {
-        x = jiki.x
-        y = jiki.y
-        if (timeCount == 30) { status = TAMA_NASI_STATE }
-    }
-
-    fun nobiru2(jiki:Jiki) {
-        //なかなかイメージ通りだけど、剣なんだから行って帰ってこないとなぁ
-
-        if (timeCount > 10) {
-            x = jiki.x
-            y = jiki.y
-            if (timeCount < 20) { nobiruhanni += 5 * timeCount }
-            if (timeCount >= 20 && timeCount < 30) { nobiruhanni -= 3 * timeCount }
-            if (timeCount == 30) { status = TAMA_NASI_STATE }
-        }
-    }
-    fun attaterukaCheck(teki:Teki):Boolean{
-        var isXInside = false
-        val tleft = teki.x -teki.ookisa / 2    //左肩
-        val tright = teki.x +teki.ookisa / 2    //右肩
-        isXInside =  (x >= tleft && x <= tright)
-        return isXInside
-    }
+    //剣のだしかたが、考えが、そもそも違う気がする。
+    //ｘ、ｙはどこの事を指すのか？
+    //剣先だ
+    //剣先と自機を繋ぐように、白いラインを表示するだけ。
+    //ｘ、ｙは数フレーム存在して、消える。当たり判定も消える。これは弾と同じ。
+    //いまはｙーnobiruhanni　とかにしているから、わかりづらくなっている。
+    //ｙは剣先の座標にする。
+    //そのように変更していこう
 
 
-    fun gotoHitState(){
-        iro.color = Color.RED
-        status = TAMA_HIT_STATE
-    }
-
-    fun hitCountSyori(){
-        hit = true //ヒットは１回のみカウントするので、すぐにfalseに
-        status = TAMA_HIT_END_STATE
-    }
-    fun motoniModosu(){
-        hit = false //ヒットは１回のみカウントするので、すぐにfalseに
-        status = TAMA_NASI_STATE
-    }
 
     //こういう感じに作ると３フレーム判定につかうので、３フレームで１ダメージみたいになる。
     fun nextFrame(jiki:Jiki,teki:Teki,isFirstMove:Boolean) {
@@ -139,6 +60,84 @@ class JikiKen(jiki:Jiki) {
             }
         }
     }
+
+    fun tatenagashikakuRectXY(): Rect {
+        val left = x  - ookisa / 2
+        val right = x  + ookisa / 2
+        if (nobiruhanni<1){nobiruhanni=0}
+        if (nobiruhanni>250){nobiruhanni=250}
+        val top = y  - nobiruhanni
+        val bottom = y
+        val m = Rect(left, top, right,bottom)
+        return m
+    }
+
+    fun draw(canvas: Canvas){
+            canvas.drawRect(tatenagashikakuRectXY(), iro)  //自機
+    }
+
+
+
+    fun jikiKaraStart(jiki:Jiki){
+        x = jiki.x
+        y = jiki.y
+        nobiruhanni = 10
+        timeCount = 0
+        hit = false
+        status =  NORMAL_STATE
+        iro.color = Color.WHITE
+    }
+
+    fun taiki(jiki:Jiki){
+        timeCount += 1
+        if(timeCount==10){
+            x = jiki.x
+            y = jiki.y
+        }
+
+    }
+
+    fun nobiru(jiki:Jiki) {
+        //なかなかイメージ通りだけど、剣なんだから行って帰ってこないとなぁ
+
+        if (timeCount > 10) {
+            x = jiki.x
+            y = jiki.y
+            if (timeCount < 20) { nobiruhanni += 5 * timeCount }
+            if (timeCount >= 20 && timeCount < 30) { nobiruhanni -= 3 * timeCount }
+            if (timeCount == 30) { status = TAMA_NASI_STATE }
+        }
+    }
+
+    fun attaterukaCheck(teki:Teki):Boolean{
+        var isXInside = false
+        val tleft = teki.x -teki.ookisa / 2    //左肩
+        val tright = teki.x +teki.ookisa / 2    //右肩
+        isXInside =  (x >= tleft && x <= tright)
+
+        var isYInside = false
+        val ttop = teki.y -teki.ookisa / 2    //上辺
+        val tbuttom = teki.y +teki.ookisa / 2    //下辺
+        isYInside =  (y  - nobiruhanni >= ttop && y  - nobiruhanni <= tbuttom)
+
+        return (isXInside && isYInside)
+    }
+
+
+    fun gotoHitState(){
+        iro.color = Color.RED
+        status = TAMA_HIT_STATE
+    }
+
+    fun hitCountSyori(){
+        hit = true //ヒットは１回のみカウントするので、すぐにfalseに
+        status = TAMA_HIT_END_STATE
+    }
+    fun motoniModosu(){
+        hit = false //ヒットは１回のみカウントするので、すぐにfalseに
+        status = TAMA_NASI_STATE
+    }
+
 
 }
 
