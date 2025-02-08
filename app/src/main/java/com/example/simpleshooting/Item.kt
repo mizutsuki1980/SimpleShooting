0e21d48a0d8b80f498f9df0b9eaf00b78d898a87
LAPTOP-7LL19PQQ\user <mizutuki1980@gmail.com>
2025-02-08 15:54:19 +0900

    Itemを変更　当たり判定を見直す あたったらエフェクトを出すようにする

--- app/src/main/java/com/example/simpleshooting/Item.kt
+++ app/src/main/java/com/example/simpleshooting/Item.kt
@@ -38,6 +38,7 @@ class Item {
     fun syokika(){
          x = xlist.random()
          y = 20
+         ookisa = initialOokisa
         status = ITEM_SYUTUGEN_STATE
     }
     fun moveOne(){
@@ -54,14 +55,17 @@ class Item {
                 ITEM_SYUTUGEN_STATE -> {
                     moveOne()
                     if (attaterukaCheck(jiki)) {
+                        ookisa = 25
                         status = ITEM_GET_STATE
                     }
                 }
                 ITEM_GET_STATE -> {
+                    ookisa /= 2
                     status = ITEM_OWARI_STATE
 
                 }
                 ITEM_OWARI_STATE -> {
+                    ookisa /= 2
                     status =  ITEM_NASI_STATE
                 }
 
@@ -70,12 +74,15 @@ class Item {
     }
 
     fun attaterukaCheck(jiki:Jiki):Boolean {
-        val x1 = jiki.x -jiki.ookisa / 2
-        val y1 = jiki.y -jiki.ookisa / 2
-        val x2 = jiki.x +jiki.ookisa / 2
-        val y2 = jiki.y +jiki.ookisa / 2
-
         //これちゃんと当たってるのかな？なんか自分の大きさが考慮されてない気がする、、、
+        //と思ったので、自分（Item）の大きさを２で割って加えてみた
+        //それっぽく動いてるように見える。
+
+        val x1 = jiki.x -jiki.ookisa / 2 - ookisa / 2
+        val y1 = jiki.y -jiki.ookisa / 2 - ookisa / 2
+        val x2 = jiki.x +jiki.ookisa / 2 + ookisa / 2
+        val y2 = jiki.y +jiki.ookisa / 2 + ookisa / 2
+
         val isXInside = (x >= x1 && x <= x2)
         val isYInside = (y >= y1 && y <= y2)
 
@@ -94,6 +101,46 @@ class Item {
 
     fun draw(canvas: Canvas){
         canvas.drawRect(shikakuRectXY(), iro)  //自機
+        if (status>2){drawyosumi(canvas)}
     }
+
+    fun drawyosumi(canvas: Canvas) {
+        //左上
+        val left1 = (x-25)  - ookisa / 2
+        val right1 = (x-25)  + ookisa / 2
+        val top1 = (y-25)  - ookisa / 2
+        val bottom1 = (y-25) + ookisa / 2
+        val m1 = Rect(left1, top1, right1,bottom1)
+        canvas.drawRect(m1, iro)  //自機
+
+        //左下
+        val left2 = (x-25)  - ookisa / 2
+        val right2 = (x-25)  + ookisa / 2
+        val top2 = (y+25)  - ookisa / 2
+        val bottom2 = (y+25) + ookisa / 2
+        val m2 = Rect(left2, top2, right2,bottom2)
+        canvas.drawRect(m2, iro)  //自機
+
+
+        //右下
+        val left3 = (x+25)  - ookisa / 2
+        val right3 = (x+25)  + ookisa / 2
+        val top3 = (y+25)  - ookisa / 2
+        val bottom3 = (y+25) + ookisa / 2
+        val m3 = Rect(left3, top3, right3,bottom3)
+        canvas.drawRect(m3, iro)  //自機
+
+
+        //右上
+        val left4 = (x+25)  - ookisa / 2
+        val right4 = (x+25)  + ookisa / 2
+        val top4 = (y-25)  - ookisa / 2
+        val bottom4 = (y-25) + ookisa / 2
+        val m4 = Rect(left4, top4, right4,bottom4)
+        canvas.drawRect(m4, iro)  //自機
+
+
+    }
+
 
 }
